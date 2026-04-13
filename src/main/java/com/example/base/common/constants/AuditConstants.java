ce3044c06d12292dab3d0e980f5294e5eb0e8bbf
ThongNguyen <thong.nq@metatechnologylab.io>
2026-04-13 15:48:16 +0700

    add multi-module and config for each module

--- src/main/java/com/example/base/common/constants/AuditConstants.java
+++
@@ -1,9 +0,0 @@
-package com.example.base.common.constants;
-
-public final class AuditConstants {
-    private AuditConstants() {
-        throw new IllegalStateException("Utility class");
-    }
-
-    public static final String SYSTEM = "SYSTEM";
-}
